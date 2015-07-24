4d3458c8ec4db7ee9025c63cbd60d96636673e79
ciruzzo <tsuchiya.yoshihiro@gmail.com>
2015-07-24 10:18:11 +0000

    changes

--- fio-freebsd.sh
+++ fio-freebsd.sh
@@ -3,7 +3,7 @@
 for t in read write 
 do
 echo $t
-fio --append-terse --output=result-freebsd-$t.txt --filename=fio-data/test --name=fio-freebsd-$t --readwrite=$t  --bs=1m freebsd.fio
+fio --append-terse --output=result-freebsd-$t.txt --filename=fio-data/test --name=fio-freebsd-$t --readwrite=$t  --bs=1m --iodepth=64 --filesize=1g
 done
 
 for t in randread randwrite 
@@ -11,7 +11,7 @@ do
 echo $t
 	for b in 4k 16k 64k
 	do
-	fio --append-terse --output=result-freebsd-$t-$b.txt --filename=fio-data/test --name=fio-freebsd-$t-$b --readwrite=$t --bs=$b freebsd.fio
+	fio --append-terse --output=result-freebsd-$t-$b.txt --filename=fio-data/test --name=fio-freebsd-$t-$b --readwrite=$t --bs=$b --thread --buffered=0 --ramp_time=30 --runtime=200 --time_based --numjobs=1 --iodepth=64 --filesize=1g
 	done
 
 done
